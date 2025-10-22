6aa117d05dbe372d4cab529e040027fe65709230
Vlad Korobko <max.vlad.kor@gmail.com>
2025-10-23 01:20:01 +0300

    Added exercises about letter cases, tabulations, new lines and spaces.

---
+++ name_cases.py
@@ -0,0 +1,26 @@
+name = "Vlad"
+print(f"Hello {name}, would you like to learn some Python today?")
+
+first_name = "Lena"
+last_name = "Korobko"
+
+print(f"{first_name.lower()} {last_name.lower()}")
+print(f"{first_name.upper()} {last_name.upper()}")
+print(f"{first_name.lower().title()} {last_name.lower().title()}")
+
+quote = "Albert Einstein once said, \"A person who never made a mistake never tried anything new.\""
+print(quote)
+
+famous_person = "Albert Einstein"
+message = f"{famous_person} once said, \"A person who never made a mistake never tried anything new.\""
+print(message)
+
+user = " Natali Korobko  "
+print(f"{user}")
+print(f"\t{user}")
+print(f"\n{user}")
+print(f"\n\t{user}")
+
+print(user.lstrip())
+print(user.rstrip())
+print(user.strip())
