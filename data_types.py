f70eddc575e511a03bc8a405a9a7d9b9ef67f81b
Vlad Korobko <max.vlad.kor@gmail.com>
2025-10-23 23:07:49 +0300

    Added two files where I wrote some exercises about numbers, float, type function and math operations

---
+++ data_types.py
@@ -0,0 +1,7 @@
+num_1 = 2
+num_2 = 2.3
+num_3 = 2 + 2.2
+
+print(f"{num_1} is {type(num_1)}")
+print(f"{num_2} is {type(num_2)}")
+print(f"{num_3} is {type(num_3)}")
---
+++ numbers_introduction.py
@@ -0,0 +1,7 @@
+print(4 + 4)
+print(9 - 1)
+print(16 / 2)
+print(-8 * -1)
+
+favourite_numb = 13
+print(f"My favourite number is {favourite_numb}")
