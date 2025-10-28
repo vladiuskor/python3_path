25a47bd61bc1e078132011930ca1c96474d2f6f8
Vlad Korobko <max.vlad.kor@gmail.com>
2025-10-28 23:43:40 +0200

    Added my first steps with lists in Python

---
+++ bicycles.py
@@ -0,0 +1,38 @@
+bicycles = ['trek', 'cannondale', 'cube', 'pride', 'redline', 'specialized', 'comanche']
+print(bicycles)
+
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[0].upper())
+print(bicycles[0].lower())
+
+print(f'\n')
+
+print(bicycles[-1])
+
+# Ex.1 Names
+names = ['Roma', 'Max', 'Vova', 'Nazar', 'Dima']
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+# Ex.2 Messages
+message_part = 'is my good friend.'
+print(f'{names[0]} {message_part}')
+print(f'{names[1]} {message_part}')
+print(f'{names[2]} {message_part}')
+print(f'{names[3]} {message_part}')
+print(f'{names[4]} {message_part}')
+
+# Ex.3 Own list
+cars = ['volvo', 'bmw', 'skoda', 'mercedes', 'ford']
+dream_text = 'I would really like to buy a'
+
+print(f'{dream_text} {cars[0].title()}')
+print(f'{dream_text} {cars[1].title()}')
+print(f'{dream_text} {cars[2].title()}')
+print(f'{dream_text} {cars[3].title()}')
+print(f'{dream_text} {cars[4].title()}')
