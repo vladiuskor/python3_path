7778a1aec91144b5cbc434fd5b14907df945f7a9
Vlad Korobko <max.vlad.kor@gmail.com>
2025-11-10 23:57:56 +0200

    Learned how to loop simple list using for in construction

---
+++ list_loop_ex.py
@@ -0,0 +1,14 @@
+pizzas = ['Madeira', 'Meat', 'Salami']
+
+for pizza in pizzas:
+    print(f"I like {pizza.lower()} pizza.")
+
+print('\nI really love pizza!!!')
+print('\n **********---**********\n')
+
+animals = ['cat', 'dog', 'fish', 'bird']
+
+for animal in animals:
+    print(f"A {animal} would make a great pet!")
+
+print('\nAny of these animals would make a great pet!')
