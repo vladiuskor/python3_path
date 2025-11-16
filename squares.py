57ac781d2e5c13e7d2f788f09170d9fd1aad9858
Vlad Korobko <max.vlad.kor@gmail.com>
2025-11-16 20:24:33 +0200

    Learned how to generate list of numbers

---
+++ squares.py
@@ -0,0 +1,10 @@
+squares = []
+for value in range(1, 11):
+    # Not a compact code bellow
+    # square = value ** 2
+    # squares.append(square)
+
+    # More compact code
+    squares.append(value ** 2)
+
+print(squares)
