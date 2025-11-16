db2a1324a2f32f23fcc41de20fa9b11aab6cf302
Vlad Korobko <max.vlad.kor@gmail.com>
2025-11-16 22:02:53 +0200

    Learned how to copy list

---
+++ foods.py
@@ -0,0 +1,11 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_food = my_foods[:]
+
+my_foods.append('cannoli')
+friend_food.append('ice cream')
+
+print('My favourite foods are:')
+print(my_foods)
+
+print("My friend's favourite foods are:")
+print(friend_food)
